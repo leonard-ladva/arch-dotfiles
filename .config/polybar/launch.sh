ca50034acbd2415ef68d30bb53a3533d046b7048
Leonard Ladva <leonard.ladva@gmail.com>
2022-11-19 22:38:40 +0200

    WIP
    working on seamless switching between laptop monitor and docked setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,8 +4,18 @@ killall polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar laptop &
 
-if [[ $(autorandr | grep 'sillamae (detected)') ]]; then
+#if [[ $(xrandr -q | grep 'DP-3-8 connected') ]]; then
+#	polybar external &
+#fi
+
+if [[ $(autorandr | grep 'sillamae (detected)') || $(autorandr | grep 'sillamae (current)') ]]; then
 	polybar external & polybar second-external
+	exit
 fi
+
+if [[ $(autorandr | grep 'home (detected)') ]]; then
+	polybar external &
+fi
+
+polybar laptop
