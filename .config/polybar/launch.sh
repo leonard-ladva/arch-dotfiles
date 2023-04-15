b34b14c9d6f2b46f7bbc47f4fa8305c6c44fb37e
Leonard Ladva <leonard.ladva@gmail.com>
2023-04-15 22:35:04 +0300

    Added all polybar files.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,22 +4,7 @@ killall polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-
-#if [[ $(xrandr -q | grep 'DP-3-8 connected') ]]; then
-#	polybar external &
-#fi
-
-if [[ $(autorandr | grep 'sillamae (detected)') || $(autorandr | grep 'sillamae (current)') ]]; then
-	polybar external & polybar second-external
-	exit
-fi
-
-#if [[ $(autorandr | grep 'home (detected)') ]]; then
-#	polybar external &
-#fi
-
-#polybar laptop
-
+# run laptop bar on all connected monitors
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload laptop &
 done
